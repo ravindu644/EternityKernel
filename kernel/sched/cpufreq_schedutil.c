c66320b1e1ddaa65ea142b49191b39f1019dd380
Vikram Mulukutla <markivx@codeaurora.org>
2017-05-04 19:31:39 -0700

    cpufreq: schedutil: Ignore work_in_progress
    Blindly ignoring frequency updates because of work_in_progress can leave
    the CPUs at the wrong frequency for a long time. It's better to update the
    frequency immediately than wait for a future event that might take a long
    time to come. The irq_work code already ignores double queuing of work. So,
    that part of the code is still safe when the work_in_progress flag is
    ignored.
    
    Change-Id: Id0b3711314dfbfa18b5f4bce30a239ee3cf962d6
    Signed-off-by: Vikram Mulukutla <markivx@codeaurora.org>
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    (cherry picked from commit 5dec9a7e188d43d9639e4c3e3315f0047f83d96a)
    (cherry picked from commit 33628fe3cd7c3d9590113901647655478e822927)

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -220,9 +220,6 @@ static bool sugov_should_update_freq(struct sugov_policy *sg_policy, u64 time)
 	    !cpufreq_this_cpu_can_update(sg_policy->policy))
 		return false;
 
-	if (sg_policy->work_in_progress)
-		return false;
-
 	if (unlikely(sg_policy->need_freq_update)) {
 		sg_policy->need_freq_update = false;
 		/*
