af89df4da5b0e5280ac07b6d058c767fd73489e5
ThunderStorms21th nalas <pinakastorm@gmail.com>
2020-08-13 10:01:57 +0200

    ble_schedutil - fix up/down timer_rate values
    Signed-off-by: ThunderStorms21th nalas <pinakastorm@gmail.com>

--- kernel/sched/cpufreq_blu_schedutil.c
+++ kernel/sched/cpufreq_blu_schedutil.c
@@ -35,8 +35,8 @@ unsigned long boosted_cpu_util(int cpu);
 #define cpufreq_enable_fast_switch(x)
 #define cpufreq_disable_fast_switch(x)
 #define SUGOV_KTHREAD_PRIORITY	50
-#define UP_RATE_LIMIT_US 5000
-#define DOWN_RATE_LIMIT_US 4000
+#define UP_RATE_LIMIT_US (500)
+#define DOWN_RATE_LIMIT_US (400)
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
