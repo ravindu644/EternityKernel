1c356c3e798b7275e0a4ae18c4ec4a358841caac
sanded <36462714+Hussein995@users.noreply.github.com>
2024-09-07 03:31:41 +0300

    Update cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -589,7 +589,6 @@ struct governor_attr {
 int cpufreq_update_freq(int cpu, unsigned int min, unsigned int max);
 
 static inline bool cpufreq_can_do_remote_dvfs(struct cpufreq_policy *policy)
-{
 	/*
 	 * Allow remote callbacks if:
 	 * - dvfs_possible_from_any_cpu flag is set
