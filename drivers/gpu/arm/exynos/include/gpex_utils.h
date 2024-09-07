10e0a634042719f0819c54c917c3619e78f419c5
Hussein995 <36462714+Hussein995@users.noreply.github.com>
2024-09-07 15:29:51 +0300

    6ce0198533716b5ffc8faeeeb59bd57439885b73

--- drivers/gpu/arm/exynos/include/gpex_utils.h
+++ drivers/gpu/arm/exynos/include/gpex_utils.h
@@ -323,6 +323,5 @@ static inline ssize_t gpex_utils_sysfs_endbuf(char *buf, ssize_t len)
  * gpu_model sysfs.
  */
 void gpex_utils_sysfs_set_gpu_model_callback(sysfs_device_read_func show_gpu_model_fn);
-int gpex_clock_update_config_data_from_dt(void);
 
 #endif /* _MALI_EXYNOS_UTILS_H_ */
