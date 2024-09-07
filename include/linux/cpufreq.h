904fe0a94e1848d8289eb9a8f4dbd9b338e18253
sanded <36462714+Hussein995@users.noreply.github.com>
2024-09-07 03:23:19 +0300

    Update cpufreq.h

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -552,7 +552,7 @@ struct cpufreq_governor *cpufreq_fallback_governor(void);
 #if defined (CONFIG_ARM_EXYNOS_FF)
 void cpufreq_policy_apply_limits(struct cpufreq_policy *policy);
 #else
-static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)
+static inline bool cpufreq_can_do_remote_dvfs(struct cpufreq_policy *policy)
 {
 	if (policy->max < policy->cur)
 		__cpufreq_driver_target(policy, policy->max, CPUFREQ_RELATION_H);
